77e5198e5419b0a22471e7d9f8166a732f007038
Bulgogi-Pizza <quitendexit@gmail.com>
2024-05-31 20:20:04 +0900

    add 240531 educations

---
+++ java-lang/app/src/main/java/study/lang/literal/Test05.java
@@ -0,0 +1,14 @@
+package study.lang.literal;
+
+//실습
+// - practice to use control character
+//
+
+public class Test05 {
+    public static void main(String[] args) {
+        // 코드를 완성하라
+        System.out.println("HelloWorld!");
+        System.out.println("Hello\r\nWorld!");
+
+    }
+}
