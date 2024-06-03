01dd41651268fd59971848b5a4abf049be2ffa05
Bulgogi-Pizza <quitendexit@gmail.com>
2024-06-03 16:38:51 +0900

    test commit

---
+++ java-lang/app/src/main/java/study/lang/literal/Test06.java
@@ -0,0 +1,13 @@
+package study.lang.literal;
+// 실습
+
+// - 다음과 같이 실행결과가 나오도록 코드를 완성하라.
+// - 컴파일된 클래스 파일을 16진수 코드로 읽어서 12.375가 2진수로 저장된 것을 확인하라
+// 
+//
+
+class Test06 {
+    public static void main(String[] args) {
+        System.out.println(12.375f);
+    }
+}
