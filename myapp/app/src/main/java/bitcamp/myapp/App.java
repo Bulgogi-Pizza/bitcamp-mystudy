4e363aba9fbd5c40106f459b25a6b1228f6baa7f
Bulgogi-Pizza <quitendexit@gmail.com>
2024-06-05 14:56:35 +0900

    add builded project file using IntelliJ

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -1,21 +1,27 @@
 package bitcamp.myapp;
 
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
-        System.out.println("[팀 프로젝트 관리 시스템]\n");
-        String menu_1 = ("1. 회원");
-        String menu_2 = ("2. 팀");
-        String menu_3 = ("3. 프로젝트");
-        String menu_4 = ("4. 게시판");
-        String menu_5 = ("5. 도움말");
-        String menu_6 = ("6. 종료");
+        String appTitle = "팀 프로젝트 관리 시스템";
+        String line = "---------------------------";
+        String[] menu = {"회원", "팀", "프로젝트", "게시판", "도움말", "종료"};
 
-        System.out.println(menu_1);
-        System.out.println(menu_2);
-        System.out.println(menu_3);
-        System.out.println(menu_4);
-        System.out.println(menu_5);
-        System.out.println(menu_6);
+        System.out.println("\u001B[1m" + line);
+        System.out.println("[" + appTitle + "]\u001B[0m\n");
+        for (int i = 0; i < menu.length; i++) {
+            if (i == 5){
+                System.out.println((i+1) + ". \u001B[31m" + menu[i] + "\u001B[0m");
+            } else {
+                System.out.println((i + 1) + ". " + menu[i]);
+            }
+        }
+        System.out.println("\u001B[1m" + line + "\u001B[0m");
+
+        Scanner a = new Scanner(System.in);
+        int b = a.nextInt();
+        System.out.println(b);
+
 
     }
 }
