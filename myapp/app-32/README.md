49262cbc69074fe4f057d7122aacd25b0c2e3f08
Bulgogi-Pizza <quitendexit@gmail.com>
2024-07-26 16:26:01 +0900

    add 2024-07-26 educations

--- myapp/app-32/README.md
+++ myapp/app-32/README.md
@@ -1,13 +1,13 @@
-# 29. File I/O API 활용하기 II :  데이터 변환 자동화 데코레이터 활용
+# 32. File I/O API 활용하기 V : JSON 형식의 텍스트로 입출력
 
 ## 학습목표
 
-- File I/O API를 사용하여 데이터를 바이너리 형식으로 출력할 수 있다.
+- JSON 파일 포맷으로 객체를 표현할 수 있다.
+- Google gson 라이브러리를 사용하여 객체를 JSON 형식으로 직렬화/역직렬화 할 수 있다.
 
 ## 요구사항
 
-- 회원, 프로젝트, 게시판 데이터를 앱을 종료할 때 파일로 저장한다.
-- 회원, 프로젝트, 게시판 데이터를 앱을 실행할 때 파일에서 읽는다.
+- 데이터를 JSON 포맷으로 파일에 저장하고 읽기
 
 ## 실행 결과
 
@@ -15,19 +15,23 @@
 
 ## 작업
 
-- 데이터를 저장
-    - App 클래스 변경
-        - loadData(), saveData() 메서드 추가
-        - loadUsers(), saveUsers() 메서드 추가
-        - loadProjects(), saveProjects() 메서드 추가
-        - loadBoards(), saveBoards() 메서드 추가
-    - User, Project, Board 클래스 변경
-        - getSeqNo(), initSeqNo() 메서드 추가
-        - getBytes(), valueOf() 메서드 추가
-
+- Google gson 라이브러리 추가
+  - build.gradle 변경
+- 데이터 로딩 및 저장 코드 변경
+  - loadJson(), saveJson() 메서드 추가
+  - loadData(), saveData() 메서드 변경
+  - loadUsers(), saveUsers() 메서드 삭제
+  - loadProjects(), saveProjects() 메서드 삭제
+  - loadBoards(), saveBoards() 메서드 삭제
+- User, Project, Board 클래스 변경
+  - SequenceNo 인터페이스 정의
+  - User, Project, Board 클래스 SequenceNo 구현
+  - 데이터 로딩 후 일련 식별 번호 초기화: initSeqNo() 메서드 추가
+ 
 ## 소스 파일
 
 - App.java
+- SequenceNo.java 추가
 - Board.java
 - Project.java
 - User.java
--- myapp/app-32/src/main/java/bitcamp/myapp/App.java
+++ myapp/app-32/src/main/java/bitcamp/myapp/App.java
@@ -20,52 +20,58 @@ import bitcamp.myapp.command.user.UserDeleteCommand;
 import bitcamp.myapp.command.user.UserListCommand;
 import bitcamp.myapp.command.user.UserUpdateCommand;
 import bitcamp.myapp.command.user.UserViewCommand;
-import bitcamp.myapp.dao.BoardDao;
-import bitcamp.myapp.dao.MapBoardDao;
-import bitcamp.myapp.dao.MapProjectDao;
-import bitcamp.myapp.dao.MapUserDao;
-import bitcamp.myapp.dao.ProjectDao;
-import bitcamp.myapp.dao.UserDao;
+import bitcamp.myapp.vo.Board;
+import bitcamp.myapp.vo.Project;
+import bitcamp.myapp.vo.SequenceNo;
+import bitcamp.myapp.vo.User;
 import bitcamp.util.Prompt;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class App {
 
+
   MenuGroup mainMenu = new MenuGroup("메인");
 
-  UserDao userDao;
-  BoardDao boardDao;
-  ProjectDao projectDao;
+  List<User> userList = new ArrayList<>();
+  List<Project> projectList = new LinkedList<>();
+  List<Board> boardList = new LinkedList<>();
 
   public App() {
 
-    userDao = new MapUserDao("data.xlsx");
-    boardDao = new MapBoardDao("data.xlsx");
-
-    ProjectMemberHandler memberHandler = new ProjectMemberHandler(userDao);
-    projectDao = new MapProjectDao("data.xlsx", memberHandler);
+    loadData();
 
     MenuGroup userMenu = new MenuGroup("회원");
-    userMenu.add(new MenuItem("등록", new UserAddCommand(userDao)));
-    userMenu.add(new MenuItem("목록", new UserListCommand(userDao)));
-    userMenu.add(new MenuItem("조회", new UserViewCommand(userDao)));
-    userMenu.add(new MenuItem("변경", new UserUpdateCommand(userDao)));
-    userMenu.add(new MenuItem("삭제", new UserDeleteCommand(userDao)));
+    userMenu.add(new MenuItem("등록", new UserAddCommand(userList)));
+    userMenu.add(new MenuItem("목록", new UserListCommand(userList)));
+    userMenu.add(new MenuItem("조회", new UserViewCommand(userList)));
+    userMenu.add(new MenuItem("변경", new UserUpdateCommand(userList)));
+    userMenu.add(new MenuItem("삭제", new UserDeleteCommand(userList)));
     mainMenu.add(userMenu);
 
     MenuGroup projectMenu = new MenuGroup("프로젝트");
-    projectMenu.add(new MenuItem("등록", new ProjectAddCommand(projectDao, memberHandler)));
-    projectMenu.add(new MenuItem("목록", new ProjectListCommand(projectDao)));
-    projectMenu.add(new MenuItem("조회", new ProjectViewCommand(projectDao)));
-    projectMenu.add(new MenuItem("변경", new ProjectUpdateCommand(projectDao, memberHandler)));
-    projectMenu.add(new MenuItem("삭제", new ProjectDeleteCommand(projectDao)));
+    ProjectMemberHandler memberHandler = new ProjectMemberHandler(userList);
+    projectMenu.add(new MenuItem("등록", new ProjectAddCommand(projectList, memberHandler)));
+    projectMenu.add(new MenuItem("목록", new ProjectListCommand(projectList)));
+    projectMenu.add(new MenuItem("조회", new ProjectViewCommand(projectList)));
+    projectMenu.add(new MenuItem("변경", new ProjectUpdateCommand(projectList, memberHandler)));
+    projectMenu.add(new MenuItem("삭제", new ProjectDeleteCommand(projectList)));
     mainMenu.add(projectMenu);
 
     MenuGroup boardMenu = new MenuGroup("게시판");
-    boardMenu.add(new MenuItem("등록", new BoardAddCommand(boardDao)));
-    boardMenu.add(new MenuItem("목록", new BoardListCommand(boardDao)));
-    boardMenu.add(new MenuItem("조회", new BoardViewCommand(boardDao)));
-    boardMenu.add(new MenuItem("변경", new BoardUpdateCommand(boardDao)));
-    boardMenu.add(new MenuItem("삭제", new BoardDeleteCommand(boardDao)));
+    boardMenu.add(new MenuItem("등록", new BoardAddCommand(boardList)));
+    boardMenu.add(new MenuItem("목록", new BoardListCommand(boardList)));
+    boardMenu.add(new MenuItem("조회", new BoardViewCommand(boardList)));
+    boardMenu.add(new MenuItem("변경", new BoardUpdateCommand(boardList)));
+    boardMenu.add(new MenuItem("삭제", new BoardDeleteCommand(boardList)));
     mainMenu.add(boardMenu);
 
     mainMenu.add(new MenuItem("도움말", new HelpCommand()));
@@ -91,16 +97,7 @@ public class App {
       ex.printStackTrace();
 
     } finally {
-      //saveData();
-      try {
-        ((MapUserDao) userDao).save();
-        ((MapBoardDao) boardDao).save();
-        ((MapProjectDao) projectDao).save();
-      } catch (Exception e) {
-        System.out.println("회원 데이터 저장 중 오류 발생!");
-        e.printStackTrace();
-        System.out.println();
-      }
+      saveData();
     }
 
     System.out.println("종료합니다.");
@@ -108,5 +105,80 @@ public class App {
     Prompt.close();
   }
 
+  private void loadData() {
+    loadJson(userList, "user.json", User.class);
+    loadJson(projectList, "project.json", Project.class);
+    loadJson(boardList, "board.json", Board.class);
+
+    System.out.println("데이터를 로딩 했습니다.");
+  }
+
+  private <E> void loadJson(List<E> list, String filename, Class<E> elementType) {
+    try (BufferedReader in = new BufferedReader(new FileReader(filename))) {
+
+      StringBuilder strBuilder = new StringBuilder();
+      String line;
+      while ((line = in.readLine()) != null) {
+        strBuilder.append(line);
+      }
+
+      list.addAll((List<E>) new GsonBuilder()
+          .setDateFormat("yyyy-MM-dd HH:mm:ss")
+          .create()
+          .fromJson(
+              strBuilder.toString(),
+              TypeToken.getParameterized(List.class, elementType)));
+
+      // 읽어 들인 객체의 타입이 SequenceNo 구현체라면
+      // 일련 번호를 객체 식별 번호로 사용한다는 것이기 때문에
+      // 목록에 저장된 객체 중에서 가장 큰 일련 번호를 알아내서 클래스의 스태틱 필드에 설정해야 한다.
+      for (Class<?> type : elementType.getInterfaces()) {
+        if (type == SequenceNo.class) {
+          initSeqNo(list, elementType);
+          break;
+        }
+      }
+
+    } catch (Exception e) {
+      System.out.printf("%s 파일 로딩 중 오류 발생!\n", filename);
+      // e.printStackTrace();
+    }
+  }
+
+  private <E> void initSeqNo(List<E> list, Class<E> elementType) throws Exception {
+    int maxSeqNo = 0;
+    for (Object element : list) {
+      SequenceNo seqObj = (SequenceNo) element;
+      if (seqObj.getNo() > maxSeqNo) {
+        maxSeqNo = seqObj.getNo();
+      }
+    }
+
+    Method method = elementType.getMethod("initSeqNo", int.class);
+    method.invoke(null, maxSeqNo);
+    // 위 코드는 다음과 같다.
+    // 예) User.initSeqNo(maxSeqNo);
+  }
+
+  private void saveData() {
+    saveJson(userList, "user.json");
+    saveJson(projectList, "project.json");
+    saveJson(boardList, "board.json");
+    System.out.println("데이터를 저장 했습니다.");
+  }
+
+  private void saveJson(Object obj, String filename) {
+    try (FileWriter out = new FileWriter(filename)) {
+
+      out.write(new GsonBuilder()
+          .setDateFormat("yyyy-MM-dd HH:mm:ss")
+          .create()
+          .toJson(obj));
+
+    } catch (IOException e) {
+      System.out.printf("%s 파일 저장 중 오류 발생!\n", filename);
+      e.printStackTrace();
+    }
+  }
 
 }
