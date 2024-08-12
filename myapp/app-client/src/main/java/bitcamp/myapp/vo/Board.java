7336005adf6d1775a2a1dfdf370fdc9ea0f5e9cd
Bulgogi-Pizza <quitendexit@gmail.com>
2024-08-12 18:00:00 +0900

    add 2024-08-12 educations

--- myapp/app-client/src/main/java/bitcamp/myapp/vo/Board.java
+++ myapp/app-client/src/main/java/bitcamp/myapp/vo/Board.java
@@ -7,7 +7,7 @@ import java.util.Objects;
 public class Board implements Serializable {
 
   private static final long serialVersionUID = 1L;
-
+ 
   private int no;
   private String title;
   private String content;
