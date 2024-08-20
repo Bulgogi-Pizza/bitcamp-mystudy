84ca4a8f394ba315aaa141544cf85d1d542127c3
Bulgogi-Pizza <quitendexit@gmail.com>
2024-08-20 18:00:01 +0900

    add 2024-08-20 educations

--- myapp/app-client/src/main/java/bitcamp/myapp/dao/mysql/UserDaoImpl.java
+++ myapp/app-client/src/main/java/bitcamp/myapp/dao/mysql/UserDaoImpl.java
@@ -1,71 +1,54 @@
 package bitcamp.myapp.dao.mysql;
 
-import bitcamp.bitbatis.SqlSession;
 import bitcamp.myapp.dao.UserDao;
 import bitcamp.myapp.vo.User;
+import org.apache.ibatis.session.SqlSession;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class UserDaoImpl implements UserDao {
 
-  private SqlSession sqlSession;
+    private SqlSession sqlSession;
 
-  public UserDaoImpl(SqlSession sqlSession) {
-    this.sqlSession = sqlSession;
-  }
+    public UserDaoImpl(SqlSession sqlSession) {
+        this.sqlSession = sqlSession;
+    }
 
-  @Override
-  public boolean insert(User user) throws Exception {
-    sqlSession.insert("insert into myapp_users(name, email, pwd, tel) values (?, ?, sha1(?), ?)",
-        user.getName(),
-        user.getEmail(),
-        user.getPassword(),
-        user.getTel());
-    return true;
-  }
+    @Override
+    public boolean insert(User user) throws Exception {
+        return sqlSession.insert("userDao.insert", user) > 0;
+    }
 
-  @Override
-  public List<User> list() throws Exception {
-    return sqlSession.selectList(
-        "select"
-            + " user_id as no,"
-            + " name,"
-            + " email"
-            + " from myapp_users order by user_id asc",
-        User.class);
-  }
+    @Override
+    public List<User> list() throws Exception {
+        return sqlSession.selectList("userDao.list");
+    }
 
-  @Override
-  public User findBy(int no) throws Exception {
-    return sqlSession.selectOne(
-        "select user_id as no, name, email, tel from myapp_users where user_id=?",
-        User.class,
-        no);
-  }
+    @Override
+    public User findBy(int no) throws Exception {
+        return sqlSession.selectOne("userDao.findBy", no);
+    }
 
-  @Override
-  public User findByEmailAndPassword(String email, String password) throws Exception {
-    return sqlSession.selectOne(
-        "select user_id as no, name, email, tel from myapp_users where email=? and pwd=sha1(?)",
-        User.class,
-        email,
-        password);
-  }
+    @Override
+    public User findByEmailAndPassword(String email, String password) throws Exception {
+        Map<String, String> emailPwdMap = new HashMap<>();
+        emailPwdMap.put("email", email);
+        emailPwdMap.put("pwd", password);
 
-  @Override
-  public boolean update(User user) throws Exception {
-    int count = sqlSession.update(
-        "update myapp_users set name=?, email=?, pwd=sha1(?), tel=? where user_id=?",
-        user.getName(),
-        user.getEmail(),
-        user.getPassword(),
-        user.getTel(),
-        user.getNo());
-    return count > 0;
-  }
+        return sqlSession.selectOne("userDao.findByEmailAndPassword", emailPwdMap);
+    }
 
-  @Override
-  public boolean delete(int no) throws Exception {
-    int count = sqlSession.delete("delete from myapp_users where user_id=?", no);
-    return count > 0;
-  }
+    @Override
+    public boolean update(User user) throws Exception {
+        int count = sqlSession.update("userDao.update", user);
+        return count > 0;
+    }
+
+    @Override
+    public boolean delete(int no) throws Exception {
+        int count = sqlSession.delete("userDao.delete", no);
+        return count > 0;
+    }
 }
--- myapp/app-client/src/main/java/bitcamp/myapp/listener/InitApplicationListener.java
+++ myapp/app-client/src/main/java/bitcamp/myapp/listener/InitApplicationListener.java
@@ -1,6 +1,5 @@
 package bitcamp.myapp.listener;
 
-import bitcamp.bitbatis.SqlSession;
 import bitcamp.context.ApplicationContext;
 import bitcamp.listener.ApplicationListener;
 import bitcamp.menu.MenuGroup;
@@ -29,10 +28,12 @@ import bitcamp.myapp.dao.UserDao;
 import bitcamp.myapp.dao.mysql.BoardDaoImpl;
 import bitcamp.myapp.dao.mysql.ProjectDaoImpl;
 import bitcamp.myapp.dao.mysql.UserDaoImpl;
-import java.io.FileReader;
+import java.io.InputStream;
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.util.Properties;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 public class InitApplicationListener implements ApplicationListener {
 
@@ -41,22 +42,14 @@ public class InitApplicationListener implements ApplicationListener {
   @Override
   public boolean onStart(ApplicationContext ctx) throws Exception {
 
-    Properties props = new Properties();
-    props.load(new FileReader("app.properties"));
-
-    String url = props.getProperty("jdbc.url");
-    String username = props.getProperty("jdbc.username");
-    String password = props.getProperty("jdbc.password");
-
-    // JDBC Connection 객체 준비
-    // => DBMS에 연결
-    con = DriverManager.getConnection(url, username, password);
-
-    SqlSession sqlSession = new SqlSession(con);
+    InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+    SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+    SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(inputStream);
+    SqlSession sqlSession = sqlSessionFactory.openSession(false);
 
     UserDao userDao = new UserDaoImpl(sqlSession);
-    BoardDao boardDao = new BoardDaoImpl(sqlSession);
-    ProjectDao projectDao = new ProjectDaoImpl(con);
+    BoardDao boardDao = new BoardDaoImpl(null);
+    ProjectDao projectDao = new ProjectDaoImpl(null);
 
     ctx.setAttribute("userDao", userDao);
     ctx.setAttribute("boardDao", boardDao);
@@ -65,11 +58,11 @@ public class InitApplicationListener implements ApplicationListener {
     MenuGroup mainMenu = ctx.getMainMenu();
 
     MenuGroup userMenu = new MenuGroup("회원");
-    userMenu.add(new MenuItem("등록", new UserAddCommand(userDao)));
+    userMenu.add(new MenuItem("등록", new UserAddCommand(userDao, sqlSession)));
     userMenu.add(new MenuItem("목록", new UserListCommand(userDao)));
     userMenu.add(new MenuItem("조회", new UserViewCommand(userDao)));
-    userMenu.add(new MenuItem("변경", new UserUpdateCommand(userDao)));
-    userMenu.add(new MenuItem("삭제", new UserDeleteCommand(userDao)));
+    userMenu.add(new MenuItem("변경", new UserUpdateCommand(userDao, sqlSession)));
+    userMenu.add(new MenuItem("삭제", new UserDeleteCommand(userDao, sqlSession)));
     mainMenu.add(userMenu);
 
     MenuGroup projectMenu = new MenuGroup("프로젝트");
